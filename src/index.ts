6bf02bb8e33bedffd347d8c056edfbef3cbffc56
Hemesh-Unka <hemeshunka@gmail.com>
2018-07-25 12:38:33 +0100

    trying out joi object validator package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as morgran from "morgan";
+import * as morgan from "morgan";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
 import { User } from "./entity/User";
@@ -12,11 +12,11 @@ createConnection().then(async connection => {
     // create express app
     const app = express();
     app.use(bodyParser.json());
-    app.use(morgran('dev'));
+    app.use(morgan('dev'));
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+        (app as any)[route.method](route.route,(req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next);
             if (result instanceof Promise) {
                 result.then((result) => {
