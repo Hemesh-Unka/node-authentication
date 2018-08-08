edf8d09a257d4acf8d6a8dc6075236a3e9b3055e
Hemesh-Unka <hemeshunka@gmail.com>
2018-08-08 13:17:04 +0100

    fixing linter errors

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import * as JWT from 'jsonwebtoken';
+import * as JWT from "jsonwebtoken";
 
 export class LoginController {
 
   public async login(request: Request, response: Response, next: NextFunction) {
     try {
-      
+
       // Generate a token
       const token = JWT.sign({
-        iss: 'API',
+        iss: "API",
         sub: request.user.uuid,
         iat: new Date().getTime(),
         exp: new Date().setDate(new Date().getDate() + 1)
@@ -25,7 +25,7 @@ export class LoginController {
     } catch (e) {
       response
         .status(400)
-        .send(e)
+        .send(e);
     }
   }
-};
+}
