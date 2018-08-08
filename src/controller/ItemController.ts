824f3e75012f0826257213043005b172c85a81be
Hemesh-Unka <hemeshunka@gmail.com>
2018-08-08 13:15:46 +0100

    refactoring code

--- src/controller/ItemController.ts
+++ src/controller/ItemController.ts
@@ -1,63 +1,49 @@
 import { Request, Response, NextFunction } from "express";
-import { RoleMiddleware } from '../middleware/accesscontrol.middleware'
+import { AuthorizeMiddleware } from "../middleware/accesscontrol.middleware";
 
-import { AccessControl } from 'accesscontrol';
 import { EndPoint } from "../utils/endpoint";
 
-
-
-
-export class ItemController {
-  public readonly all: EndPoint = {
-    authorize: async (request: Request, response: Response, next: NextFunction) => {},
-    method: this._all
-  };
-
-  private async _all(request: Request, response: Response, next: NextFunction) {
+class ItemInterface {
+  async all(request: Request, response: Response, next: NextFunction) {
     try {
-      // Begin dirty code
-      const grants = await RoleMiddleware.permit();
-      const ac = new AccessControl(grants);
-
-      const permission = ac.can(request.user.role.role_name).readAny('items');
-
-      // Debugging
-      console.log('resource: ', permission.resource)
-      console.log('roles:', permission.roles)
-      console.log('attributes: ', permission.attributes)
-
-      if (!permission.granted) { throw ({ error: 'Permission denied.' }) }
-
       response
         .send([
           {
             item_id: 1,
-            item_name: 'Some Item Name',
+            item_name: "Some Item Name",
             sku: 12345
           }
-        ])
+        ]);
     } catch (e) {
       response
         .status(400)
-        .send(e)
+        .send(e);
     }
   }
 
-  public readonly create: EndPoint = {
-    authorize: async (request: Request, response: Response, next: NextFunction) => {},
-    method: this._create
-  };
-
-  async _create(request: Request, response: Response, next: NextFunction) {
+  async create(request: Request, response: Response, next: NextFunction) {
     try {
       response
-        .send(request.body)
+        .send(request.body);
     } catch (e) {
       response
         .status(400)
-        .send(e)
+        .send(e);
     }
   }
 }
 
+export class ItemController {
+  private itemInterface: ItemInterface = new ItemInterface();
+  private authorize: AuthorizeMiddleware = new AuthorizeMiddleware();
 
+  public readonly all: EndPoint = {
+    authorize: this.authorize.authorize("read", "items"),
+    method: this.itemInterface.all
+  };
+
+  public readonly create: EndPoint = {
+    authorize: this.authorize.authorize("create", "items"),
+    method: this.itemInterface.create
+  };
+}
