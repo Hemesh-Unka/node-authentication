a7580d8cb2c74627ccea51b4f645f854d44d2815
Hemesh-Unka <hemeshunka@gmail.com>
2018-08-08 16:09:36 +0100

    added test config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export = [
   {
@@ -11,6 +11,32 @@ export = [
     "synchronize": true,
     "dropSchema": false,
     "logging": false,
+    "entities": [
+      "src/entity/**/*.ts"
+    ],
+    "migrations": [
+      "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+      "src/subscriber/**/*.ts"
+    ],
+    "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+    }
+  },
+  {
+    "name": "test",
+    "type": "postgres",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "postgres",
+    "synchronize": true,
+    "logging": false,
+    "dropSchema": true,
     "entities": [
       "src/entity/**/*.ts"
     ],
