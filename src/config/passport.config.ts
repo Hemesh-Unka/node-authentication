952f6b1ff00bd37b416fe5d8052408d0a3b5d20e
Hemesh-Unka <hemeshunka@gmail.com>
2018-08-08 15:05:24 +0100

    linter fixes

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -1,23 +1,23 @@
-import 'dotenv/config';
-import * as passport from 'passport';
-import * as passportJWT from 'passport-jwt';
-import * as passportLocal from 'passport-local';
-import { getRepository } from 'typeorm';
-import { User } from '../entity/User';
+import "dotenv/config";
+import * as passport from "passport";
+import * as passportJWT from "passport-jwt";
+import * as passportLocal from "passport-local";
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
 
 passport.use(new passportJWT.Strategy({
-  jwtFromRequest: passportJWT.ExtractJwt.fromHeader('authorization'),
+  jwtFromRequest: passportJWT.ExtractJwt.fromHeader("authorization"),
   secretOrKey: process.env.JWT_SECRET
 }, async (payload: any, next: any) => {
 
   try {
     // Find the user specified in the token
-    const user = await getRepository(User).findOne({ where: { uuid: payload.sub}, relations: ['role'] });
+    const user = await getRepository(User).findOne({ where: { uuid: payload.sub}, relations: ["role"] });
 
-    //If user does not exist
+    // If user does not exist
     if (!user) {
       return next(null, false);
-    };
+    }
 
     // Return the user
     next(null, user);
@@ -27,24 +27,24 @@ passport.use(new passportJWT.Strategy({
 }));
 
 passport.use(new passportLocal.Strategy({
-  usernameField: 'email'
+  usernameField: "email"
 }, async (email: string, password: string, next: any) => {
 
   try {
     // Find the user from the email
-    const user = await getRepository(User).findOne({ email })
+    const user = await getRepository(User).findOne({ email });
 
     // If user does not exist
     if (!user) {
-      return next(null, false, { message: 'Incorrect username.' });
-    };
+      return next(null, false, { message: "Incorrect username." });
+    }
 
     // Check if password is correct
     const isMatch = await user.validatePassword(password);
 
     // If not correct password
     if (!isMatch) {
-      return next(null, false, { message: 'Incorrect password.' });
+      return next(null, false, { message: "Incorrect password." });
     }
 
     // Return the user
