5de0aea61be37f9d00fc1f6c238dfc654a779e34
Hemesh-Unka <hemeshunka@gmail.com>
2018-08-13 09:39:31 +0100

    refactoring tests

--- src/tests/register.spec.ts
+++ src/tests/register.spec.ts
@@ -1,5 +1,4 @@
 import { expect } from "chai";
-import "mocha";
 import * as request from "supertest";
 import { startServer } from "../bootstrap";
 
@@ -8,15 +7,15 @@ const password = "superadmin";
 const assigned_role = "superadmin";
 
 before(async () => {
-  const app = await startServer();
+  await startServer();
 });
-
 
 describe("Register", () => {
   describe("POST /register", () => {
     it("it should register a new user", async () => {
       const response = await request("http://localhost:3000")
         .post("/register")
+        .set("Accept", "application/json")
         .send({
           email,
           password,
@@ -28,9 +27,10 @@ describe("Register", () => {
       expect(response.body).to.have.property("token");
     });
 
-    it("it should not be able to register the same new user", async () => {
+    it("it should not be able to register another user using the same password", async () => {
       const response = await request("http://localhost:3000")
         .post("/register")
+        .set("Accept", "application/json")
         .send({
           email,
           password,
@@ -39,7 +39,7 @@ describe("Register", () => {
 
       expect(response.status).to.equal(404);
       expect(response.type).to.equal("application/json");
-      expect(response.body).to.include({ "error": "Email is already in use." });
+      expect(response.body).to.deep.equal({ "error": "Email is already in use." });
     });
   });
 });
